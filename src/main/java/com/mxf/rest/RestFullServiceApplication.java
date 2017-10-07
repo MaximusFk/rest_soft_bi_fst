00d08e0d82d20c65616e9d4a8c08b375d1bc3cd2
Max Legelbakh <maximusfk@gmail.com>
2017-10-07 08:15:45 +0300

    Initial Commit

---
+++ src/main/java/com/mxf/rest/RestFullServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mxf.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullServiceApplication.class, args);
+	}
+}
