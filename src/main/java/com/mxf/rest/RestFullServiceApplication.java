18b2b7335792e87fd1e6496f08e60b14f5d091fa
Max Legelbakh <maximusfk@gmail.com>
2017-10-08 09:30:51 +0300

    Added annotation for scan components

--- src/main/java/com/mxf/rest/RestFullServiceApplication.java
+++ src/main/java/com/mxf/rest/RestFullServiceApplication.java
@@ -1,9 +1,14 @@
 package com.mxf.rest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan("com.mfx.rest")
 public class RestFullServiceApplication {
 
 	public static void main(String[] args) {
